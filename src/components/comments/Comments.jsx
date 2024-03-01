b6ef802be7a5444681d4d6af7beb79c43439986c
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2024-03-01 12:35:43 -0500

    single post page design

---
+++ src/components/comments/Comments.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./comments.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Comments = () => {
+  const status = "authenticated";
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Comments</h1>
+      {status === "authenticated" ? (
+        <div className={styles.write}>
+          <textarea placeholder="Write a comment..." className={styles.input} />
+          <button className={styles.button}>Send</button>
+        </div>
+      ) : (
+        <Link href="/login>">Login to Write a Comment</Link>
+      )}
+      <div className={styles.comments}>
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>01.01.2024</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            This is description.This is description.This is description.This is
+            description.This is description.This is description.This is
+            description.This is description.This is description.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
