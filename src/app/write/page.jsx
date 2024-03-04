2c40b8b54e1dd0bc2f87c0a4bdf803a8f2aafea8
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2024-03-04 14:40:03 -0500

    connect to backend(to be continued)

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -6,10 +6,25 @@ import Image from "next/image";
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 const WritePage = () => {
+  const { status } = useSession();
+
+  const router = useRouter();
+
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <input type="text" placeholder="Title" className={styles.input} />
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -4,19 +4,22 @@ import React from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
 import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
-  //temporary
-  const status = "not authenticated";
+
+  const { status } = useSession();
   return (
     <>
-      {status === "not authenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login">Login</Link>
       ) : (
         <>
           <Link href="/write">Write</Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
