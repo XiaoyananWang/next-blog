6b140eaff0083eb8203ce45a32018cede8a1cbe9
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2024-03-05 11:45:44 -0500

    post page data fetch

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,27 +3,29 @@ import Image from "next/image";
 import styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ key, item }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-      </div>
+    <div className={styles.container} key={key}>
+      {item.img && (
+        <div className={styles.imageContainer}>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+      )}
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>Culture</span>
+          <span className={styles.date}>
+            {item.createdAt.substring(0, 10)} -{" "}
+          </span>
+          <span className={styles.category}>{item.catSlug}</span>
         </div>
-        <Link href="/">
-          <h1>Title</h1>
+        <Link href={`/posts/${item.slug}`}>
+          <h1>{item.title}</h1>
         </Link>
 
-        <p className={styles.desc}>
-          Descriptionsiageajkalbjhczlieugbsjbg vlkczjvhueialgbwgjkdbvk
-          jzhglkjehgzkjbvjdbzkjDescriptionsia geajkalbjhczlieugbsjbgvl
-          kczjvhueialgbwgjkdbvkjzhglkjehgz kjbvjdbzkjDescriptionsiageajkalbjhcz
-        </p>
-        <Link href="/" className={styles.link}>Read More</Link>
+        <p className={styles.desc}>{item.desc.substring(0, 60)}</p>
+        <Link href={`/posts/${item.slug}`} className={styles.link}>
+          Read More
+        </Link>
       </div>
     </div>
   );
